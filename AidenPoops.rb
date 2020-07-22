31bbc05622dd95e00bb38fbad6be9b5f4bbaf190
James Fox <auron1731@yahoo.com>
2020-07-21 20:08:31 -0600

    remote add

---
+++ AidenPoops.rb
@@ -0,0 +1,10 @@
+print "What is your name?"
+name = gets.chomp
+
+print "What is your favorite game??"
+game = "Minecraft Duhhhhhhh"
+
+print "What is your favorite food???"
+food = gets.chomp
+
+puts "Your name is #{name}, your favorite game is #{game}, and you LOOOOOOOOOOOOOVE #{food}!!!"
